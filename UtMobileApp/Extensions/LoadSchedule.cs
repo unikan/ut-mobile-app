7315c96449228722cda1974a3676a7d896154eee
Neshat Ajruli <neshatajruli@gmail.com>
2019-10-08 11:08:08 +0200

    Saving it in a state where it does not have error

---
+++ UtMobileApp/Extensions/LoadSchedule.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using static UtMobileApp.Models.ScheduleJSON;
+
+namespace UtMobileApp.Extensions
+{
+    class LoadSchedule
+    {
+        public async Task<List<Entry>> DeserializeJsonAsync(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(string.Format(url));
+                string result = await response.Content.ReadAsStringAsync();
+                RootObject root = JsonConvert.DeserializeObject<RootObject>(result);
+
+                // Remove 5 rows of unnecessary content
+                root.feed.entry.RemoveRange(0, 5);
+                return root.feed.entry;
+            }
+        }
+
+        public IEnumerable<Entry> GetDay(string day, string semester, List<Entry> schedule)
+        {
+            IEnumerable<Entry> results = schedule.Where(i => (i.Semester.t == semester) && (i.Day.t == day));
+
+            return results;
+        }
+    }
+}
--- UtMobileApp/Views/Schedule.xaml.cs
+++ UtMobileApp/Views/Schedule.xaml.cs
@@ -54,14 +54,7 @@ namespace UtMobileApp.Views
             schedule.DataSource = scheduleAppointmentCollection;
 
 
-            base.OnAppearing(); 
-        }
-
-
-        public static DateTime StartOfWeek(this DateTime dt, DayOfWeek startOfWeek)
-        {
-            int diff = (7 + (dt.DayOfWeek - startOfWeek)) % 7;
-            return dt.AddDays(-1 * diff).Date;
+            base.OnAppearing();
         }
     }
 }
