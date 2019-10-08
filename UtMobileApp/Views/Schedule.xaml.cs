f7df4ba468e79b1aa0c4e8f58c72a9c535f60c89
Neshat Ajruli <neshatajruli@gmail.com>
2019-10-08 11:04:17 +0200

    Trying to save the project

--- UtMobileApp/Views/Schedule.xaml.cs
+++ UtMobileApp/Views/Schedule.xaml.cs
@@ -17,22 +17,51 @@ namespace UtMobileApp.Views
     {
         public Schedule()
         {
+            NavigationPage.SetHasBackButton(this, false);
             InitializeComponent();
         }
 
         protected override async void OnAppearing()
         {
-            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
-            sw.Start();
             string url = "https://spreadsheets.google.com/feeds/list/1SFGzFIq8K7va4HzT9PZUwRNxch_yqZItn80-Kwu-u6c/3/public/values?alt=json";
-            var LoadSchedule = new Functions.LoadSchedule();
+            var LoadSchedule = new Extensions.LoadSchedule();
+            List<Models.ScheduleJSON.Entry> scheduleList = await LoadSchedule.DeserializeJsonAsync(url);
 
-            lsa.ItemsSource = await LoadSchedule.DeserializeJsonAsync(url);
-            sw.Stop();
+            // Create 6 lists for everyday Monday to Saturday, for actual semester
+            IEnumerable<Models.ScheduleJSON.Entry> MondayList = LoadSchedule.GetDay("e hënë", "I", scheduleList);
+            IEnumerable<Models.ScheduleJSON.Entry> TuesdayList = LoadSchedule.GetDay("e martë", "I", scheduleList);
+            IEnumerable<Models.ScheduleJSON.Entry> WednesdayList = LoadSchedule.GetDay("e mërkurë", "I", scheduleList);
+            IEnumerable<Models.ScheduleJSON.Entry> ThursdayList = LoadSchedule.GetDay("e enjte", "I", scheduleList);
+            IEnumerable<Models.ScheduleJSON.Entry> FridayList = LoadSchedule.GetDay("e premte", "I", scheduleList);
+            IEnumerable<Models.ScheduleJSON.Entry> SaturdayList = LoadSchedule.GetDay("e shtunë", "I", scheduleList);
 
-            label.Text = sw.ElapsedMilliseconds.ToString();
+
+
+
+
+            // Creating an instance for schedule appointment collection
+            ScheduleAppointmentCollection scheduleAppointmentCollection = new ScheduleAppointmentCollection();
+            //Adding schedule appointment in schedule appointment collection 
+            scheduleAppointmentCollection.Add(new ScheduleAppointment()
+            {
+                StartTime = new DateTime(2019, 05, 08, 10, 0, 0),
+                EndTime = new DateTime(2017, 05, 08, 12, 0, 0),
+                Subject = "Meeting",
+                Location = "Hutchison road",
+            });
+
+            //Adding schedule appointment collection to DataSource of SfSchedule
+            schedule.DataSource = scheduleAppointmentCollection;
+
 
             base.OnAppearing(); 
+        }
+
+
+        public static DateTime StartOfWeek(this DateTime dt, DayOfWeek startOfWeek)
+        {
+            int diff = (7 + (dt.DayOfWeek - startOfWeek)) % 7;
+            return dt.AddDays(-1 * diff).Date;
         }
     }
 }
